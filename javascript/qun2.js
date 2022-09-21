1b69f8ebb2dff1dac54a0a6b1131cd82cd9f762c
pragatibhandari <bhandariprag7@gmail.com>
2022-09-21 13:26:30 +0300

    file name changed

--- javascript/qun2.js
+++
@@ -1,9 +0,0 @@
-// Create a prompt to where you can write twitter like text limited to 10 words.
-
-function limitText(){
-var text = "My nane is nanandndnanan nananananananan n"
-return(
-  console.log(alert(text))
-)
-}
-limitText();
