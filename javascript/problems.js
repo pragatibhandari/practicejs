b2e8d95ceda7056b1279362d39f57efa35c9b3c3
pragatibhandari <bhandariprag7@gmail.com>
2022-09-21 13:40:08 +0300

    made some comment

--- javascript/problems.js
+++ javascript/problems.js
@@ -1,5 +1,6 @@
+// Problem 1.
 // Program that prints 1 to 100
-// But for multiple of 3 print Fizz
+// for multiple of 3 print Fizz
 // for multiple of 5 print Buzz
 // for multiple of both 3 and 5 print FizzBuzz
 
@@ -18,14 +19,14 @@ function printnum() {
 }
 printnum();
 
+// Problem 2
 // Create a prompt where you can write twitter like text limited to 10 words.
 
 function limitText() {
   var text = prompt("Write your tweet");
   var shortText = text.slice(0, 11);
   return console.log(shortText);
-
-  // 1 line solution to above function.
 }
 
+// 1 line solution for problem 2
 console.log(prompt("Write a short tweet").slice(0, 11));
