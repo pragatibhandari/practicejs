38cdfd5dc0dbb100b0967b245aa94fee84fca2d4
pragatibhandari <bhandariprag7@gmail.com>
2022-10-11 10:47:31 +0300

    problem 3 completed

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -1,13 +1,13 @@
 /* Problem 1
 Write code for a function that gets an multidimensional array as an input parameter and prints out the item that is found on the third column of the second row. */
 
+function getItem23(matrix) {
+  console.log(matrix[1][2]);
+}
 let matrix = [
   ["a", "b", "c"],
   ["d", "e", "f"],
 ];
-function getItem23(matrix) {
-  console.log(matrix[1][2]);
-}
 getItem23(matrix); //f
 
 /* Problem 2
@@ -23,3 +23,13 @@ function average(array) {
 }
 let numbers = [1, 5, 1, 5, 10, 12, 44]; // 11.142857142857142
 average(numbers);
+
+/* Problem 3
+You have an array with the following items: 'apple', 'orange', 'banana', 'kiwi'.
+Write code for a function that prints out the element from the array from the given index. */
+
+let array = ["apple", "orange", "banana", "kiwi"];
+function getItem(index) {
+  console.log(array[index]);
+}
+getItem(3); //kiwi
