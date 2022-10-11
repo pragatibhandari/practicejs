792b36013a59dbb056597c9fafe8d06f35441e40
pragatibhandari <bhandariprag7@gmail.com>
2022-10-11 09:41:11 +0300

    problem 4 added

--- javascript/conditionals.js
+++ javascript/conditionals.js
@@ -54,3 +54,32 @@ function negativeOrPositive(number) {
 negativeOrPositive(0); // Number is zero
 negativeOrPositive(1); // Number is positive
 negativeOrPositive(-1); // Number is negative
+
+// Problem 4
+/* Write code for a function that checks whether a given string is a valid email (NOTE! the validation doesn't need to perfect, 
+it's enough that it fulfills the requirements listed here).
+The requirements are:
+String must contain following characters: ".", "@"
+There must be at least two characters after the period.
+The first letter cannot be a "." or a "@". */
+
+function checkEmail(email) {
+  var re = /[A-Z]/gim;
+  var [email1, email2] = email.split(".");
+  if (
+    email.includes("@") &&
+    email.includes(".") &&
+    email2.length >= 2 &&
+    email.charAt(0) != "@" &&
+    email.charAt(0) != "." &&
+    re.test(email2) == true
+  ) {
+    console.log("Email ok");
+  } else {
+    console.log("Email invalid");
+  }
+}
+
+checkEmail("foo"); //Email invalid
+checkEmail("email@test.fi"); //Email ok
+checkEmail("email@test."); //Email invalid
