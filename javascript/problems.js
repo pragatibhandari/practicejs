e65593a08841502ed0f80e5f68dffd18f523948c
pragatibhandari <bhandariprag7@gmail.com>
2022-10-17 23:48:01 +0300

    some more problems

--- javascript/problems.js
+++ javascript/problems.js
@@ -102,3 +102,43 @@ process.stdout.write("\n--Array sorted--");
 for (let i = 0; i < fruits.length; i++) {
   process.stdout.write("\n" + fruits[i]);
 }
+
+// Create a function hello()
+function hello() {
+  // that prints out text "Hello"
+  process.stdout.write("Hello");
+}
+
+// Call the function
+hello();
+
+// Create a function that takes two numbers as input
+function sum(number1, number2) {
+  // calculates the sum of the two numbers
+  let sum = number1 + number2;
+  number1 = 1;
+  // prints out the result
+  process.stdout.write("\n" + number1 + " + " + number2 + " = " + sum);
+}
+
+// Call the function to test it
+let number1 = 2;
+let number2 = 5;
+sum(number1, number2);
+process.stdout.write("Value of the original variable is " + number1);
+sum(28, 59);
+
+// Create a function that takes two numbers as input
+function sum2(number1, number2) {
+  // calculates the sum of the two numbers
+  let sum = number1 + number2;
+  // return the result
+  return sum;
+}
+
+// Call the function
+let result = sum2(10, 10);
+process.stdout.write("\nSum is " + result);
+process.stdout.write("\nSum is " + sum2(100, 5));
+let average = sum2(10, 55, 456, 10) / 4;
+process.stdout.write("\nAverage is " + average);
