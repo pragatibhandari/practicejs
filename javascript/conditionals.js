f973e06db49d69eb30fcf03d76eb1fedf1257faf
pragatibhandari <bhandariprag7@gmail.com>
2022-10-17 23:56:27 +0300

    conditionals.js file changed

--- javascript/conditionals.js
+++ javascript/conditionals.js
@@ -110,3 +110,19 @@ calculate(8, 12, "+"); // 20
 calculate(8, 12, "-"); // -4
 calculate(8, 12, "*"); // 96
 calculate(10, 5, "/"); //2
+
+// Ask the user to input a number.
+process.stdout.write("Please enter a number: ");
+// Read the input given by the user and store it in a variable
+process.stdin.on("data", function (inputFromUser) {
+  // The statements below will be executed automatically after
+  // the user has typed in something.
+  let enteredNumber = Number(inputFromUser);
+  if (enteredNumber >= 1 && enteredNumber <= 10) {
+    process.stdout.write("Entered number is between 1-10");
+  }
+  if (enteredNumber < 1 || enteredNumber > 10) {
+    process.stdout.write("Entered number is outside 1-10");
+  }
+  process.exit();
+});
