34c12c7cca183b7b7b1dfbe136e780deec8a47d9
pragatibhandari <bhandariprag7@gmail.com>
2022-10-31 23:57:56 +0200

    some more problems solved

--- javascript/problems.js
+++ javascript/problems.js
@@ -142,3 +142,40 @@ process.stdout.write("\nSum is " + result);
 process.stdout.write("\nSum is " + sum2(100, 5));
 let average = sum2(10, 55, 456, 10) / 4;
 process.stdout.write("\nAverage is " + average);
+
+let numbers = [];
+process.stdout.write(
+  "\n\nPlease enter a number (zero terminates the program): "
+);
+
+process.stdin.on("data", function (inputFromUser) {
+  let userInput = inputFromUser.toString().trim();
+
+  if (userInput == 0) {
+    // Find the largest value in the array using a loop and print it out
+    let largest = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > largest) {
+        largest = numbers[i];
+      }
+    }
+    process.stdout.write("\nLargest number is " + largest + "\n");
+
+    // Find the smallest value in the array using a loop and print it out
+    let smallest = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] < smallest) {
+        smallest = numbers[i];
+      }
+    }
+    process.stdout.write("Smallest number is " + smallest + "\n\n");
+    process.exit();
+  } else {
+    // Add the number into array
+    numbers.push(userInput);
+    // Keep asking for numbers
+    process.stdout.write(
+      "\n\nPlease enter a number (zero terminates the program): "
+    );
+  }
+});
